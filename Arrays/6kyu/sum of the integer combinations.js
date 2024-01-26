db7d8147982be41a17d515223ab28e40d44ca71b
JustSha10 <justsha10101010@gmail.com>
2024-01-26 21:22:18 +0300

    26.01 1.2

---
+++ Arrays/6kyu/sum of the integer combinations.js
@@ -0,0 +1,22 @@
+// DESCRIPTION:
+// Consider the array [3,6,9,12]. If we generate all 
+// the combinations with repetition that sum to 12, 
+// we get 5 combinations: [12], [6,6], [3,9], [3,3,6], [3,3,3,3]. 
+// The length of the sub-arrays (such as [3,3,3,3] should be 
+//     less than or equal to the length of the initial array ([3,6,9,12]).
+
+// Given an array of positive integers and a number n, count 
+// all combinations with repetition of integers that sum to n. For example:
+
+// find([3,6,9,12],12) = 5.
+// More examples in the test cases.
+
+// Good luck!
+
+
+function find(arr, value, depth = arr.length) {
+    if (value == 0) return 1;
+    if (depth == 0) return 0;
+    return arr.reduce((sum, number, index) => 
+        sum + find(arr.slice(0, index + 1), number - value, depth - 1), 0)
+  }
